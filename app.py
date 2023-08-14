039881cd07cd13053cabb1693597667426814eb4
mavey14 <harveylencio@gmail.com>
2023-08-14 23:47:43 +0800

    add background and ground

--- app.py
+++ app.py
@@ -4,18 +4,27 @@ from sys import exit
 
 
 
-width = 800
-height = 400
+width = 450
+height = 670
 screen = pygame.display.set_mode((width,height))
-pygame.display.set_caption('Flappy Bird') #set title 
-pygame.display.set_icon(pygame.image.load('./imgs/bird1.png')) #set icon
-
+pygame.display.set_caption('Flappy Bird')  #set title 
+pygame.display.set_icon(pygame.image.load('imgs/bird1.png')) #set icon
+clock = pygame.time.Clock()
+background = pygame.image.load('imgs/bg.png') # Load the image
+resized_background = pygame.transform.scale(background,(width,height))
+ground = pygame.image.load('imgs/ground.png')
+resized_ground =  pygame.transform.scale(ground,(width,100))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            exit() #to break while true
-        
+           
+            exit()  #to break while true
+
+    # Draw the resized image 
+    screen.blit(resized_background,(0,0)) #image , x,y
+    screen.blit(resized_ground,(0,screen.get_height() - resized_ground.get_height()))
     pygame.display.update()
+    clock.tick(60)   # Limit the frame rate to 60 FPS
     
