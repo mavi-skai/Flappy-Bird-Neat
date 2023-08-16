6578083e332f95bd325ae90bbcf263a965155188
mavey14 <harveylencio@gmail.com>
2023-08-17 03:53:12 +0800

    add class for bird

--- app.py
+++ app.py
@@ -1,7 +1,62 @@
 import pygame
 pygame.init()
 from sys import exit
-import random
+import random 
+
+
+class Bird(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.jumping = True
+        self.bird_fly_1 = pygame.image.load('imgs/bird1.png').convert_alpha()
+        self.bird_fly_2 = pygame.image.load('imgs/bird2.png').convert_alpha()
+        self.bird_fly_3 = pygame.image.load('imgs/bird3.png').convert_alpha()
+        self.bird_fly = [self.bird_fly_1,self.bird_fly_2,self.bird_fly_3] #array of sprite
+        self.bird_index = 0 #to increment sprite
+
+        self.image = self.bird_fly[self.bird_index] #set sprite
+        self.image = pygame.transform.scale(self.image,(self.image.get_width()+5,self.image.get_height()+5)) #not working properly
+        self.rect = self.image.get_rect(center = (200,300))
+        self.bird_gravity = 0.5
+        
+
+
+    def bird_input(self):
+        keys = pygame.key.get_pressed()
+        if(keys[pygame.K_SPACE] and self.jumping == True):
+            self.bird_gravity = -10
+            self.jumping = False
+
+        if not keys[pygame.K_SPACE]:
+            self.jumping = True
+
+    def destroy(self):
+        if (self.rect.x<=-80):
+            self.kill()
+
+
+    def apply_gravity(self):
+        self.bird_gravity+=0.7
+        self.rect.y += self.bird_gravity
+        if(self.rect.bottom > screen.get_height() - ground_scale.get_height()):
+            self.rect.y = screen.get_height() - ground_scale.get_height() - self.image.get_height()
+
+    def animation(self):
+        self.bird_index+=0.1
+        if(self.bird_index>len(self.bird_fly)):
+            self.bird_index = 0
+        self.image = self.bird_fly[int(self.bird_index)]
+        
+
+    def update(self):
+        self.animation()
+        self.bird_input()
+        self.apply_gravity()
+
+   
+
+
 
 def pipe_movement(pipe_list):
     if pipe_list:
@@ -21,14 +76,7 @@ def pipe_collision(bird,pipe_list):
                 return False
     return True
     
-
-def bird_animation():
-    global bird_scale, bird_index
-
-    bird_index += 0.1
-    if(bird_index>len(bird_fly)):
-        bird_index = 0
-    bird_scale = bird_fly[int(bird_index)]
+    
 
 width = 500
 height = 670
@@ -52,18 +100,11 @@ Generation = font.render('Generation: ',False,'Blue') #create surface for font
 text_rect = Generation.get_rect(center = (50,30))
 
 #bird
-bird = pygame.image.load('imgs/bird1.png').convert_alpha()
-bird_fly_1 = pygame.image.load('imgs/bird1.png').convert_alpha()
-bird_fly_2 = pygame.image.load('imgs/bird2.png').convert_alpha()
-bird_fly_3 = pygame.image.load('imgs/bird3.png').convert_alpha()
-bird_fly = [bird_fly_1,bird_fly_2,bird_fly_3]
-bird_index = 0
-bird_surf = bird_fly[bird_index]
-bird_scale = pygame.transform.scale(bird_surf,(bird_surf.get_width()+10,bird_surf.get_height() + 10))
-bird_rect = bird_scale.get_rect(topleft = (200,300))
-bird_gravity = 0.5
+bird = pygame.sprite.GroupSingle()
+bird.add(Bird())
 
 #pipe
+pipe = pygame.sprite.Group()
 pipe = pygame.image.load('imgs/pipe.png').convert_alpha()
 pipe_scale = pygame.transform.scale(pipe,(80,200))
 pipe_rect = pipe_scale.get_rect(topleft = (670,200))
@@ -78,6 +119,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()  #to break while true
+
         if game_active:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
@@ -85,6 +127,7 @@ while True:
 
             if event.type == pipe_timer:
                 pipe_list.append(pipe_scale.get_rect(topleft = (670,200)))
+
     
     if game_active:
         # Background, ground and fonts
@@ -96,16 +139,12 @@ while True:
 
         #pipe
         pipe_list = pipe_movement(pipe_list)
-        game_active = pipe_collision(bird_rect,pipe_list)
+        #game_active = pipe_collision(bird_rect,pipe_list)
 
         #Bird gravity
-        bird_gravity += 0.7
-        bird_rect.y += bird_gravity
-        if(bird_rect.bottom > screen.get_height() - ground_scale.get_height()):
-            bird_rect.y = screen.get_height() - ground_scale.get_height() - bird_scale.get_height()
-        bird_animation()
-        screen.blit(bird_scale,bird_rect)
-
+        bird.draw(screen) 
+        bird.update() 
+  
     
     # if(bird_rect.collidedict(pipe_rect)):  colliding to pipe
     #     pass
