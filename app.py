07fbfadabde59d6ba644abfbc1c02754deb35d49
mavey14 <harveylencio@gmail.com>
2023-08-18 10:54:59 +0800

    fix bird class

--- app.py
+++ app.py
@@ -3,67 +3,21 @@ pygame.init()
 from sys import exit
 import random 
 from pipe import Pipe
-
-class Bird(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.jumping = True
-        self.bird_fly_1 = pygame.image.load('imgs/bird1.png').convert_alpha()
-        self.bird_fly_2 = pygame.image.load('imgs/bird2.png').convert_alpha()
-        self.bird_fly_3 = pygame.image.load('imgs/bird3.png').convert_alpha()
-        self.bird_index = 0 #to increment sprite
-        # Scale factor for the bird images
-        scale_factor = 1.2 
-        # Scale the bird images
-        self.bird_fly_1 = pygame.transform.scale(self.bird_fly_1, 
-                                                 (int(self.bird_fly_1.get_width() * scale_factor), 
-                                                  int(self.bird_fly_1.get_height() * scale_factor)))
-        self.bird_fly_2 = pygame.transform.scale(self.bird_fly_2, 
-                                                 (int(self.bird_fly_2.get_width() * scale_factor), 
-                                                  int(self.bird_fly_2.get_height() * scale_factor)))
-        self.bird_fly_3 = pygame.transform.scale(self.bird_fly_3, 
-                                                 (int(self.bird_fly_3.get_width() * scale_factor), 
-                                                  int(self.bird_fly_3.get_height() * scale_factor)))
-        
-        self.bird_fly = [self.bird_fly_1,self.bird_fly_2,self.bird_fly_3] #array of sprite
-        self.image = self.bird_fly[self.bird_index] #set sprite
-        self.rect = self.image.get_rect(center = (200,300))
-        self.bird_gravity = 0.5
-        
-
-    def bird_input(self):
-        keys = pygame.key.get_pressed()
-        if(keys[pygame.K_SPACE] and self.jumping == True):
-            self.bird_gravity = -10
-            self.jumping = False
-
-        if not keys[pygame.K_SPACE]:
-            self.jumping = True
-
-    def destroy(self):
-        if (self.rect.x<=-80):
-            self.kill()
-
-    def apply_gravity(self):
-        self.bird_gravity+=0.7
-        self.rect.y += self.bird_gravity
-        if(self.rect.bottom > screen.get_height() - ground_scale.get_height()):
-            self.rect.y = screen.get_height() - ground_scale.get_height() - self.image.get_height()
-
-    def animation(self):
-        self.bird_index+=0.1
-        if(self.bird_index>len(self.bird_fly)):
-            self.bird_index = 0
-        self.image = self.bird_fly[int(self.bird_index)]
-        
-    def update(self):
-        self.animation()
-        self.bird_input()
-        self.apply_gravity()
-
+from bird import Bird
    
 def collision_sprite():
-    pygame.sprite.spritecollide(bird.sprite,pipe_group,False) #if true pipe will be deleted false no
+    global game_active
+
+    for bird in bird_group:
+        bird_mask = pygame.mask.from_surface(bird.image)
+        for pipe in pipe_group:
+            pipe_mask = pygame.mask.from_surface(pipe.image)
+            if bird_mask.overlap(pipe_mask, (pipe.rect.x - bird.rect.x, pipe.rect.y - bird.rect.y)):
+                print('collide')
+                game_active = False
+
+#if(pygame.sprite.groupcollide(bird_group,pipe_group,False,False)):
+#pygame.draw.rect(screen,'Pink',bird_rect)
 
 width = 500
 height = 670
@@ -73,12 +27,12 @@ pygame.display.set_icon(pygame.image.load('imgs/bird1.png')) #set icon
 clock = pygame.time.Clock()
 game_active = True
 
-#background
+#Image load
 background = pygame.image.load('imgs/bg.png').convert_alpha() # Load the image
-background_scale = pygame.transform.scale(background,(width,height))
-
-#ground
 ground = pygame.image.load('imgs/ground.png').convert_alpha()
+
+#Scale
+background_scale = pygame.transform.scale(background,(width,height))
 ground_scale =  pygame.transform.scale(ground,(width,100))
 
 #fonts
@@ -87,8 +41,8 @@ Generation = font.render('Generation: ',False,'Blue') #create surface for font
 text_rect = Generation.get_rect(center = (50,30))
 
 #bird
-bird = pygame.sprite.GroupSingle()
-bird.add(Bird())
+bird_group = pygame.sprite.Group()
+bird_group.add(Bird())
 
 #pipe
 pipe_group = pygame.sprite.Group()
@@ -120,21 +74,21 @@ while True:
     if game_active:
         #background
         screen.blit(background_scale,(0,0)) #image , x,y
+
         #pipe
-        pipe_group.draw(screen)
+        pipe_group.draw(screen) 
         pipe_group.update()
+
         #ground and fonts
         screen.blit(ground_scale,(0,screen.get_height() - ground_scale.get_height()))
         screen.blit(Generation,text_rect)
 
-        #pygame.draw.rect(screen,'Pink',bird_rect)
-
-        
-        #pipe_group.update(1)
-
         #Bird gravity
-        bird.draw(screen) 
-        bird.update() 
+        bird_group.draw(screen) 
+        bird_group.update(screen) 
+
+        #collision
+        collision_sprite()
   
     
     pygame.display.flip()
---
+++ bird.py
@@ -0,0 +1,59 @@
+import pygame
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.jumping = True
+        self.bird_fly_1 = pygame.image.load('imgs/bird1.png').convert_alpha()
+        self.bird_fly_2 = pygame.image.load('imgs/bird2.png').convert_alpha()
+        self.bird_fly_3 = pygame.image.load('imgs/bird3.png').convert_alpha()
+        self.bird_index = 0 #to increment sprite
+        # Scale factor for the bird images
+        scale_factor = 1.2 
+        # Scale the bird images
+        self.bird_fly_1 = pygame.transform.scale(self.bird_fly_1, 
+                                                 (int(self.bird_fly_1.get_width() * scale_factor), 
+                                                  int(self.bird_fly_1.get_height() * scale_factor)))
+        self.bird_fly_2 = pygame.transform.scale(self.bird_fly_2, 
+                                                 (int(self.bird_fly_2.get_width() * scale_factor), 
+                                                  int(self.bird_fly_2.get_height() * scale_factor)))
+        self.bird_fly_3 = pygame.transform.scale(self.bird_fly_3, 
+                                                 (int(self.bird_fly_3.get_width() * scale_factor), 
+                                                  int(self.bird_fly_3.get_height() * scale_factor)))
+        
+        self.bird_fly = [self.bird_fly_1,self.bird_fly_2,self.bird_fly_3] #array of sprite
+        self.image = self.bird_fly[self.bird_index] #set sprite
+        self.rect = self.image.get_rect(center = (200,300))
+        self.bird_gravity = 0.5
+        
+
+    def bird_input(self):
+        keys = pygame.key.get_pressed()
+        if(keys[pygame.K_SPACE] and self.jumping == True):
+            self.bird_gravity = -10
+            self.jumping = False
+
+        if not keys[pygame.K_SPACE]:
+            self.jumping = True
+
+
+    def destroy(self):
+        if (self.rect.x<=-80):
+            self.kill()
+
+    def apply_gravity(self,screen):
+        self.bird_gravity+=0.7
+        self.rect.y += self.bird_gravity
+        if(self.rect.bottom > screen.get_height() - 100):
+            self.rect.y = screen.get_height() - 100 - self.image.get_height()
+
+    def animation(self):
+        self.bird_index+=0.1
+        if(self.bird_index>len(self.bird_fly)):
+            self.bird_index = 0
+        self.image = self.bird_fly[int(self.bird_index)]
+        
+    def update(self,screen):
+        self.animation()
+        self.bird_input()
+        self.apply_gravity(screen)
